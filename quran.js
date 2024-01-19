8c0a1f62976ef4b1a660f1c73c837cff0a01d35e
root <root@DESKTOP-3S5SIJL.localdomain>
2024-01-19 10:17:52 +0300

    feat: add a function called displayAllChaptersInfo that displays list of chapter and their informations and get called in getAllChapters function

--- quran.js
+++ quran.js
@@ -1,11 +1,41 @@
 const chapters = document.querySelector(".chapters");
+const searchInput = document.querySelector("#search");
+const pageLoader = document.querySelector("#loader");
+
+const displayAllChaptersInfo = (
+  chapterInArabicName,
+  chapterInEnglishName,
+  englishNameTranslation,
+  TotalVersesInChapter,
+  chapterNumber
+) => {
+  chapters.innerHTML += `
+  <div class='chapter-info'>
+    <div class='chapter-names'>
+      <a href='' id='chapter-name-en'>
+        ${chapterInEnglishName}
+      </a>
+      <a href='' id='chapter-name-ar'>
+        ${chapterInArabicName}
+      </a>
+    </div>
+    <div class='chapter-description' style='margin-top: 45px'>
+      <p id='about-chapter'>${englishNameTranslation}</p>
+      <p id='total-verses'>
+        ${TotalVersesInChapter}
+      </p>
+    </div>
+    <span id='chapter-verse-number'>${chapterNumber}</span>
+  </div>`;
+};
+
+//search/filter chapters
 
 let baseApiUrl = "http://api.alquran.cloud/v1/quran/quran-uthmani";
 const getAllChapters = async () => {
-  pageLoader.style.display = "block";
   let response = await fetch(baseApiUrl);
   let chapter = await response.json();
-  pageLoader.style.display = "none";
+
   chapter.data.surahs.forEach((chapter) => {
     displayAllChaptersInfo(
       chapter.name,
